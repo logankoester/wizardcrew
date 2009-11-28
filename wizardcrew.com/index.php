c6ea42368aa0f0aeb2492f6f36557a67d07a004c
Logan Koester <logan@logankoester.com>
2009-11-28 02:43:56 -0500

    Added wizardcrew.com static website

---
+++ wizardcrew.com/index.php
@@ -0,0 +1,33 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<title>Wizard Crew Promotions - Underground night life in New England</title>
+
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=utf-8" />
+<meta name="robots" content="index, follow" />
+
+<meta name="description" content="Underground night life in New England" />
+<meta name="keywords" content="" />
+
+<link rel="shortcut icon" href="images/favicon_wizard.ico" />
+<link rel="stylesheet" href="style.css" media="screen,projection" type="text/css" />
+<link rel="alternate" href="" title="RSS Feed" type="application/rss+xml" />
+
+<script src="js/jquery.js"> type="text/javascript"></script>
+<script type="text/javascript">
+</script>
+
+</head>
+<body>
+<div id="container">
+	<img id="wizard" src="images/logo-600x303.png" />
+  <br />
+  <tt>[<a href="mailto:rave@wizardcrew.com">Send us an Email</a>]</tt>
+  <br /><br /><tt>or...</tt><br /><br />
+  <!-- Google Call Widget -->
+  <object type="application/x-shockwave-flash" data="https://clients4.google.com/voice/embed/webCallButton" width="230" height="85"><param name="movie" value="https://clients4.google.com/voice/embed/webCallButton" /><param name="wmode" value="transparent" /><param name="FlashVars" value="id=35ca0ab60cd47d766f1eb87cc8fec0b029d64f4c&style=0" /></object>
+  <br />
+  <tt><strong>+1 (803) 2-RAVERS</strong></tt>
+</div>
+</body>
+</html>
